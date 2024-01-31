1caa5227f8ff73a411cc271accf9b0e3c3f557e5
yoishi <75917699+yumadao@users.noreply.github.com>
2024-01-31 15:43:44 +0900

    learn pointer

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"learn-go/calculator"
 	"os"
+	"unsafe"
 
 	"github.com/joho/godotenv"
 )
@@ -14,4 +15,31 @@ func main() {
 	fmt.Println(calculator.Offset)
 	fmt.Println(calculator.Sum(1, 2))
 	fmt.Println(calculator.Multiply(1, 2))
+
+	var ui1 uint16
+	fmt.Printf("memory address of ui1: %p\n", &ui1)
+	var ui2 uint16
+	fmt.Printf("memory address of ui1: %p\n", &ui2)
+	var p1 *uint16
+	fmt.Printf("value of p1: %v\n", p1)
+	p1 = &ui1
+	fmt.Printf("value of p1: %v\n", p1)
+	fmt.Printf("size of p1: %d[bytes]\n", unsafe.Sizeof(p1))
+	fmt.Printf("memory address of p1: %p\n", &p1)
+	fmt.Printf("value of ui1(dereference): %v\n", *p1)
+	*p1 = 1
+	fmt.Printf("value of ui1: %v\n", ui1)
+
+	var pp1 **uint16 = &p1
+	fmt.Printf("value of pp1: %v\n", pp1)
+
+	ok, result := true, "A"
+	if ok {
+		result := "B"
+		println(result)
+	} else {
+		result := "C"
+		println(result)
+	}
+	println(result)
 }
