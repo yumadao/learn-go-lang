1ab26bf4c410a29816ef2cfa74d5defa0a4ecf98
yoishi <75917699+yumadao@users.noreply.github.com>
2024-02-02 00:58:43 +0900

    wip

--- db/db.go
+++ db/db.go
@@ -18,7 +18,7 @@ func NewDB() *gorm.DB {
 		}
 	}
 	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PW"), os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DB"))
-	db, err := gorm.Open(postgres.Open(url), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(url), new(gorm.Config))
 	if err != nil {
 		log.Fatalln(err)
 	}
