e5f556a7cb352f2dde5051f9c31f56cdc09d78ac
yoishi <75917699+yumadao@users.noreply.github.com>
2024-02-03 21:05:48 +0900

    impl user api

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"learn-go/controller"
+	"learn-go/db"
+	"learn-go/repository"
+	"learn-go/router"
+	"learn-go/usecase"
+)
 
+func main() {
+	db := db.NewDB()
+	userRepository := repository.NewUserRepository(db)
+	userUsecase := usecase.NewUserUsecase(userRepository)
+	userController := controller.NewUserController(userUsecase)
+	e := router.NewRouter(userController)
+	e.Logger.Fatal(e.Start(":8080"))
 }
