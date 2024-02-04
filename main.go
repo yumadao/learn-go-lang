58194e8732754799acca2f016500763e004fd756
yoishi <75917699+yumadao@users.noreply.github.com>
2024-02-04 23:27:39 +0900

    impl validation

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"learn-go/repository"
 	"learn-go/router"
 	"learn-go/usecase"
+	"learn-go/validator"
 )
 
 func main() {
@@ -13,13 +14,15 @@ func main() {
 	db := db.NewDB()
 
 	// user
+	userValidator := validator.NewUserValidator()
 	userRepository := repository.NewUserRepository(db)
-	userUsecase := usecase.NewUserUsecase(userRepository)
+	userUsecase := usecase.NewUserUsecase(userRepository, userValidator)
 	userController := controller.NewUserController(userUsecase)
 
 	// task
+	taskValidator := validator.NewTaskValidator()
 	taskRepository := repository.NewTaskRepository(db)
-	taskUsecase := usecase.NewTaskUsecase(taskRepository)
+	taskUsecase := usecase.NewTaskUsecase(taskRepository, taskValidator)
 	taskController := controller.NewTaskController(taskUsecase)
 
 	e := router.NewRouter(userController, taskController)
