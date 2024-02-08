8ac5e4fd7539f5e0a63e676585c8798d73f32600
yoishi <75917699+yumadao@users.noreply.github.com>
2024-02-09 00:34:53 +0900

    fix health check without middleware

--- router/router.go
+++ router/router.go
@@ -13,12 +13,18 @@ import (
 func NewRouter(uc controller.IUserController, tc controller.ITaskController) *echo.Echo {
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		Skipper: func(c echo.Context) bool {
+			return c.Path() == "/health"
+		},
 		AllowOrigins:     []string{"http://localhost:3000", os.Getenv("FE_URL")},
 		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAccessControlAllowHeaders, echo.HeaderXCSRFToken},
 		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE"},
 		AllowCredentials: true,
 	}))
 	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+		Skipper: func(c echo.Context) bool {
+			return c.Path() == "/health"
+		},
 		CookiePath:     "/",
 		CookieDomain:   os.Getenv("API_DOMAIN"),
 		CookieHTTPOnly: true,
