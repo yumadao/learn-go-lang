43201b61ebe5a05adcd14a95b8714f079a0d05a4
yoishi <75917699+yumadao@users.noreply.github.com>
2024-02-15 23:19:38 +0900

    add settings.json

--- usecase/task_usecase.go
+++ usecase/task_usecase.go
@@ -8,7 +8,7 @@ import (
 
 type ITaskUsecase interface {
 	GetAllTasks(userId uint) ([]model.TaskResponse, error)
-	GetTaskById(userId uint, taskid uint) (model.TaskResponse, error)
+	GetTaskById(userId uint, taskId uint) (model.TaskResponse, error)
 	CreateTask(task model.Task) (model.TaskResponse, error)
 	UpdateTask(task model.Task, userId uint, taskId uint) (model.TaskResponse, error)
 	DeleteTask(userId uint, taskId uint) error
