257590d5a5004724c09db8d490caa58f2d08dee2
yoishi <75917699+yumadao@users.noreply.github.com>
2024-02-19 21:25:48 +0900

    fix validator

--- router/router.go
+++ router/router.go
@@ -16,7 +16,7 @@ func NewRouter(uc controller.IUserController, tc controller.ITaskController) *ec
 		Skipper: func(c echo.Context) bool {
 			return c.Path() == "/health"
 		},
-		AllowOrigins:     []string{"http://localhost:3000", os.Getenv("FE_URL")},
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173", "http://localhost:8788", os.Getenv("FE_URL")},
 		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAccessControlAllowHeaders, echo.HeaderXCSRFToken},
 		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE"},
 		AllowCredentials: true,
--- validator/task_validator.go
+++ validator/task_validator.go
@@ -17,5 +17,5 @@ func NewTaskValidator() ITaskValidator {
 }
 
 func (tv *taskValidator) TaskValidate(task model.Task) error {
-	return validation.ValidateStruct(&task, validation.Field(&task.Title, validation.Required.Error("title is required"), validation.RuneLength(1, 10).Error("limited max 10 chars")))
+	return validation.ValidateStruct(&task, validation.Field(&task.Title, validation.Required.Error("title is required"), validation.RuneLength(1, 64).Error("limited max 64 chars")))
 }
